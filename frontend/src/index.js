fe74ed62aa6f908b2594afee750c482716ba155c
rcvsms2012 <rcvsms2012@gmail.com>
2022-01-22 09:17:10 +0530

    all change

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./store";
+//import store from "./store";
 
 import { positions, transitions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
@@ -14,11 +14,10 @@ const options = {
 };
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider>
     <AlertProvider template={AlertTemplate} {...options}>
       <App />
     </AlertProvider>
   </Provider>,
   document.getElementById("root")
 );
-
